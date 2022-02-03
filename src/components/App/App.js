dea06eb23dde6f7bd2aaf1f6d2e7d8d4a23a12d9
Sahil <mail@sahilkhanna.org>
2022-02-03 11:20:53 +1100

    -Able to get back user detail

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import "./App.css";
 import Login from "../login/login";
 import Dashboard from "../dashboard/dashboard";
+import Welcome from "../welcome/welcome";
 
 function App() {
   const [token, setToken] = useState();
@@ -12,10 +13,12 @@ function App() {
   return (
     <div className="App">
       <div className="wrapper">
-        <h1>Application</h1>
+        <h1>Rainmaker IoT Dashboard</h1>
+        <Welcome auth={token} />
         <Router>
           <Routes>
-            <Route path="/" element={<Dashboard />} />
+            <Route path="/" element={<h1>Welcome</h1>} />
+            <Route path="/dashboard" element={<Dashboard />} />
           </Routes>
         </Router>
       </div>
